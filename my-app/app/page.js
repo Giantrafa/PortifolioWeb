fd4e6a0c34f21fc4977397958e3a704824b863d7
Rafael Assis <giantrafa@gmail.com>
2026-05-08 12:37:01 +0000

    estreia

---
+++ my-app/app/page.js
@@ -0,0 +1,20 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div> 
+    </div>
+  );
+}
+/*
+colocar estilo no CSS
+usar uma API (qualquer uma)
+
+ter pelo menos as abas
+home
+Sobre(Com todas tecnologias / Modulos que voce utilizou para desenvolver)
+experiencia academica
+experiencia proficional
+projetos desenvolvidos
+
+*/
